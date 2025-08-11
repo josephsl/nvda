929606bb0ee0a841f8307dc913a9a1c0c942bce2
Joseph Lee <joseph.lee22590@gmail.com>
2025-08-10 20:49:04 -0600

    UIA handler: set minimum IUIAutomation interface version to IUIAutomation4 (Windows 10 Version 1607)

--- source/UIAHandler/__init__.py
+++ source/UIAHandler/__init__.py
@@ -471,8 +471,8 @@ class UIAHandler(COMObject):
 			oledll.ole32.CoInitializeEx(None, comtypes.COINIT_MULTITHREADED)
 			self.clientObject = CoCreateInstance(
 				UIA.CUIAutomation8._reg_clsid_,
-				# Minimum interface is IUIAutomation3 (Windows 8.1).
-				interface=UIA.CUIAutomation8._com_interfaces_[1],
+				# Minimum interface is IUIAutomation4 (Windows 10 Version 1607).
+				interface=UIA.CUIAutomation8._com_interfaces_[2],
 				clsctx=CLSCTX_INPROC_SERVER,
 			)
 			# #7345: Instruct UIA to never map MSAA winEvents to UIA propertyChange events.
